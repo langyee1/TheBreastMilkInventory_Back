3a488361a043cb1f4f5f341b5e9506f1d80c2d12
Angelica <angelica.yee57@gmail.com>
2023-08-14 16:11:50 -0500

    changed init to render

--- app/__init__.py
+++ app/__init__.py
@@ -15,12 +15,12 @@ def create_app(test_config=None):
 
     if not test_config:
 
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-            "SQLALCHEMY_DATABASE_URI")
+        #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("RENDER_DATABASE_URI")
     else:
         app.config["TESTING"] = True
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_TEST_DATABASE_URI")
+        #app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("RENDER_DATABASE_URI")
 
     db.init_app(app)
     migrate.init_app(app, db)
